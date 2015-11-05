1d4a1b960d0a897087c52661846bde2a214e5ada
Сергей <bugsmafia@gmail.com>
2015-11-05 22:57:32 +0500

    ц

--- www/js/script.js
+++ www/js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 $debug = 1; // Отладка
 $SpeedUpd = 5000;
 
 function windowSize(){
-	$windowsW = $(document).width(); // Ширина экрана
-	$windowsH = $(document).height(); // Высота экрана
-	$bodyW = $('body').width(); // Ширина рабочей области
-	$bodyH = $('body').height(); // Высота рабочей области
-	$headerH = $('header').height(); // Высота шапки
+	$windowsW = jQuery(document).width(); // Ширина экрана
+	$windowsH = jQuery(document).height(); // Высота экрана
+	$bodyW = jQuery('body').width(); // Ширина рабочей области
+	$bodyH = jQuery('body').height(); // Высота рабочей области
+	$headerH = jQuery('header').height(); // Высота шапки
 	
-	$TrackTitleH = $('#track-title').height(); // Высота шапки	
-	$TrackSongH = $('#track-song').height(); // Высота шапки
+	$TrackTitleH = jQuery('#track-title').height(); // Высота шапки	
+	$TrackSongH = jQuery('#track-song').height(); // Высота шапки
 
-	$VolumeH = $('#Volume').height(); // Уровня громкости
+	$VolumeH = jQuery('#Volume').height(); // Уровня громкости
 	
-	$ContPlayL = $('#cont-play-left').width(); //Страница: Статус вопроизведения. Левый блок: Ширина.
-	$ContPlayC = $('#cont-play-center').width(); //Страница: Статус вопроизведения. Центральный блок: Ширина.
-	$ContPlayR = $('#cont-play-right').width(); //Страница: Статус вопроизведения. Правый блок: Ширина.
+	$ContPlayL = jQuery('#cont-play-left').width(); //Страница: Статус вопроизведения. Левый блок: Ширина.
+	$ContPlayC = jQuery('#cont-play-center').width(); //Страница: Статус вопроизведения. Центральный блок: Ширина.
+	$ContPlayR = jQuery('#cont-play-right').width(); //Страница: Статус вопроизведения. Правый блок: Ширина.
 	
 	$BigPlayBtn = $windowsW - $ContPlayL - $ContPlayR - 60; // Расчитываем ширину кнопки\изображения статус стрека
 	$BigPlayBtn = $BigPlayBtn - 60;
@@ -24,16 +24,16 @@ function windowSize(){
 	$DigPlayBtnHeaderHFix = $windowsH - $headerH;
 	if ($DigPlayBtnHeaderHFix <= $BigPlayBtn) {
 		$DigPlayBtnHeaderHFix = $DigPlayBtnHeaderHFix - $TrackTitleH - $TrackSongH - 30;
-		$('#status-track-img').css('width',$DigPlayBtnHeaderHFix);
-		$('#status-track-img').css('height',$DigPlayBtnHeaderHFix);
-		$('#play-btn-line').css('line-height',$DigPlayBtnHeaderHFix+'px');
+		jQuery('#status-track-img').css('width',$DigPlayBtnHeaderHFix);
+		jQuery('#status-track-img').css('height',$DigPlayBtnHeaderHFix);
+		jQuery('#play-btn-line').css('line-height',$DigPlayBtnHeaderHFix+'px');
 		if ($debug = 1){
 			//console.log('Высота кнопки больше. Формула #1');
 		}
 	} else {
-		$('#status-track-img').css('width',$BigPlayBtn);
-		$('#status-track-img').css('height',$BigPlayBtn);
-		$('#play-btn-line').css('line-height',$BigPlayBtn+'px');
+		jQuery('#status-track-img').css('width',$BigPlayBtn);
+		jQuery('#status-track-img').css('height',$BigPlayBtn);
+		jQuery('#play-btn-line').css('line-height',$BigPlayBtn+'px');
 		if ($debug = 1){
 			//console.log('Высота кнопки меньше. Формула #2');
 		}
@@ -41,15 +41,15 @@ function windowSize(){
 	
 	$bgW = $windowsW / 2;
 	$bgH = ($windowsH / 2) + $headerH - 30;
-	$('#bg').css('left',$bgW);
-	$('#bg').css('top',$bgH);
+	jQuery('#bg').css('left',$bgW);
+	jQuery('#bg').css('top',$bgH);
 	if ($debug = 1){
 		//console.log('Размер экрана изменился - '+$windowsW+' x '+$windowsH);
 		//console.log('Размер области изменились - '+$bodyW+' x '+$bodyH);
 	}
 }
 function StatusTrackUpdate(){
-	$.getJSON("http://app.radio13.ru/status/json.php?i=l", function(load) {
+	jQuery.getJSON("http://app.radio13.ru/status/json.php?i=l", function(load) {
 		$id = load.id;
 		$a = load.a;
 		$s = load.s;
@@ -60,32 +60,32 @@ function StatusTrackUpdate(){
 			console.log('Обновление статуса: #'+$id+' '+$a+' - '+$s+'. База #'+$c+'. VK audio: '+$vkmus);
 		}
 		
-		$('#track-title').text($a);
-		$('#track-song').html('</br>'+$s);
-		$('#stats-track-name').html('<strong>'+$a+'</strong> - '+$s);
-		$('body').attr('id', $id);
+		jQuery('#track-title').text($a);
+		jQuery('#track-song').html('</br>'+$s);
+		jQuery('#stats-track-name').html('<strong>'+$a+'</strong> - '+$s);
+		jQuery('body').attr('id', $id);
 	
-		$.ajax({
+		jQuery.ajax({
 			url: "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist="+$a+"&album="+$s+"&api_key=88571316d4e244f24172ea9a9bf602fe",
 			type: "GET",
 			dataType: "xml",
 			success: function(xml) {				
-				$(xml).find('album').each(function(){
-					$(this).find('image').each(function(){
-						var img = $(this).attr('size');
+				jQuery(xml).find('album').each(function(){
+					jQuery(this).find('image').each(function(){
+						var img = jQuery(this).attr('size');
 						if(img == "mega")
-							if ($(this).text()){
-								$('#status-track-img').css('background-image', 'url("'+$(this).text()+'")');
+							if (jQuery(this).text()){
+								jQuery('#status-track-img').css('background-image', 'url("'+jQuery(this).text()+'")');
 							}
 							else {
-								$('#status-track-img').css('background-image', 'url("images/no-image.png")');
+								jQuery('#status-track-img').css('background-image', 'url("images/no-image.png")');
 							}
 					})
 				});
 			},
 			statusCode: {
 				400: function() {
-					$('#status-track-img').css('background-image', 'url("images/no-image.png")');
+					jQuery('#status-track-img').css('background-image', 'url("images/no-image.png")');
 					
 				}
 			}
@@ -95,13 +95,13 @@ function StatusTrackUpdate(){
 	windowSize();
 }
 
-$(window).load(windowSize); // при загрузке
-$(window).resize(windowSize); // Выполняем каждый раз при имзенении размера экрана
-$(window).load(StatusTrackUpdate);
+jQuery(window).load(windowSize); // при загрузке
+jQuery(window).resize(windowSize); // Выполняем каждый раз при имзенении размера экрана
+jQuery(window).load(StatusTrackUpdate);
 $i = 0;
 $id = 1;
 setInterval(function(){
-	$.getJSON("http://app.radio13.ru/status/json.php?i=i", function(info) {
+	jQuery.getJSON("http://app.radio13.ru/status/json.php?i=i", function(info) {
 		
 		$i = info.i; // Уникальный ID текущего трека
 		$l = info.l; // Лайк
@@ -113,10 +113,10 @@ setInterval(function(){
 		$sv = parseFloat($l) + parseFloat($u);
 		if($sv != 0){
 			var prc = Math.round(($l * 100) / $sv);
-			$('#like-line').width(prc+'%');
+			jQuery('#like-line').width(prc+'%');
 		}
 		else {
-			$('#like-line').width('0%');
+			jQuery('#like-line').width('0%');
 		}			
 	});
 	windowSize();	
@@ -125,13 +125,13 @@ setInterval(function(){
 
 
 $playerLoad = 0;
-$( "#status-track-img" ).click(function() {
+jQuery( "#status-track-img" ).click(function() {
 	if ($playerLoad == 0){
-		$('#audio').html('<audio id="audioplay" controls preload="none"><source src="http://play.radio13.ru" type="audio/mpeg"></audio>');
+		jQuery('#audio').html('<audio id="audioplay" controls preload="none"><source src="http://play.radio13.ru" type="audio/mpeg"></audio>');
 		var audio = document.getElementById("audioplay");
 		audio.play();
 		$playerLoad++;
-		$('#status-track-img').html('');
+		jQuery('#status-track-img').html('');
 		if ($debug = 1){
 			console.log('Плеер загружен');
 		}
@@ -140,8 +140,8 @@ $( "#status-track-img" ).click(function() {
 		audio.pause();
 		audio.src = ""; // Stops audio download.
 		audio.load(); // Initiate a new load, required in Firefox 3.x.
-		//  $('#audio').html('STOP');		
-		$('#status-track-img').html('<div class="play-btn-stop"><div id="play-btn-line">ВКЛЮЧИТЬ</div></div>');
+		//  jQuery('#audio').html('STOP');		
+		jQuery('#status-track-img').html('<div class="play-btn-stop"><div id="play-btn-line">ВКЛЮЧИТЬ</div></div>');
 		if ($debug = 1){
 			$playerLoad = 0;
 			console.log('Плеер отключен');			
@@ -151,12 +151,12 @@ $( "#status-track-img" ).click(function() {
 });
 
 // Управление громкостью
-$('#volume').click(function(e) {
-	$volumeH = $('#volume').width(); // Ширина блока с модулем управления
-    var posX = $(this).position().left,posY = $(this).position().top; // Координаты клика
+jQuery('#volume').click(function(e) {
+	$volumeH = jQuery('#volume').width(); // Ширина блока с модулем управления
+    var posX = jQuery(this).position().left,posY = jQuery(this).position().top; // Координаты клика
 	$volumeFinal = (e.pageX - posX) * 100;
 	$volumeFinal = $volumeFinal / $volumeH;
-	$('#volume-line').css('width', $volumeFinal+'%');
+	jQuery('#volume-line').css('width', $volumeFinal+'%');
 	$volumeFinal = $volumeFinal / 100;
 	$volumeFinal = $volumeFinal.toFixed(2);
 	var audio = document.getElementById("audioplay"); // Ищем наш плеер
